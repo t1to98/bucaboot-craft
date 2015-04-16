edcea8b8caf510f76d296c56b0e486bded29d9c2
Tito Bottitta <tito@upstatement.com>
2015-04-16 00:22:59 -0400

    First pass at homepage slideshows

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -6,8 +6,11 @@
 
     var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
-      direction: 'vertical',
-      loop: true
+      direction: 'horizontal',
+      loop: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      spaceBetween: 30
     })
 
 })(jQuery);
